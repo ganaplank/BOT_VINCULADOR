2a540fb0218119be24aba3dde012ac2376a5bdd0
Victor <victor@example.com>
2026-05-26 11:01:36 -0300

    Snapshot: Vinculador standalone backup before hub migration

---
+++ codigo iframe pesquisa.pl
@@ -0,0 +1,43 @@
+
+
+
+
+<html>
+
+<head>
+<title>Webware Painel Webm&iacute;nio</title>
+
+</head>
+
+<LINK href="estilo.css" rel=stylesheet type=text/css>
+   <LINK href="../gerenciamento/custom-painel-webware-interno.css" rel=stylesheet type=text/css>
+<body >
+
+
+ <h3> <a href='javascript:history.go(-1)'><img src='/bin/images/voltar.png'  width='17px'></a> Webmínio - Pesquisa</h3><div class='container-conteudo-interno'>
+		<br>
+		<table>
+		<tr>
+            <th rowspan="2">Condominio</th>
+			<th rowspan="2">Nome do Usuário</th>
+			<th rowspan="2">E-mail</th>
+			<th rowspan="2">Login</th>
+            <th colspan="3">Inativo</th>
+         </tr>
+         <tr>
+            <th>Data</th>
+            <th>Usuário</th>
+            <th>Nome</th>
+		</tr>
+		<tr><td><a href=/bin/aplic/usuario.asp?u=7023953>00551 - CONDOMÍNIO MADEN VILA MADALENA GERAL</a></td><td><a href=/bin/aplic/usuario.asp?u=7023953>ISCHIA S/A</a></td><td><a href=/bin/aplic/usuario.asp?u=7023953>vickileardi@terra.com.br</a></td><td><a href=/bin/aplic/usuario.asp?u=7023953>JI65DA7727</a></td><td>27/02/2026 16:27:17</td><td>BARBARA.ARIETA </td><td>Barbara Tomaz Arieta</td></tr><tr><td><a href=/bin/aplic/usuario.asp?u=7024808>00551 - CONDOMÍNIO MADEN VILA MADALENA GERAL</a></td><td><a href=/bin/aplic/usuario.asp?u=7024808>ISCHIA S/A</a></td><td><a href=/bin/aplic/usuario.asp?u=7024808>vickileardi@terra.com.br</a></td><td><a href=/bin/aplic/usuario.asp?u=7024808>PA43TO7727</a></td><td>15/04/2026 09:43:08</td><td>VICTOR_NORONHA </td><td>Victor Noronha</td></tr><tr><td><a href=/bin/aplic/usuario.asp?u=7028081>00552 - MADEN VILA MADALENA RESIDENCIAL</a></td><td><a href=/bin/aplic/usuario.asp?u=7028081>Vitoria Anna Rita Leardi de Souza Costa</a></td><td><a href=/bin/aplic/usuario.asp?u=7028081>vickileardi@terra.com.br</a></td><td><a href=/bin/aplic/usuario.asp?u=7028081>VICKILEARDI</a></td><td></td><td> </td><td></td></tr>		
+	
+	</table>
+    <div>Total : 3</div>
+	<br>
+	
+</div>
+
+
+
+</body>
+</html>
